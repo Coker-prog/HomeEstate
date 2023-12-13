8d9e230875a950e1c3256d40ac1d0b138c115293
Coker <gbolahancoker99@gmail.com>
2023-12-13 16:27:10 +0100

    new addittion

--- src/component/Header/Header.jsx
+++ src/component/Header/Header.jsx
@@ -3,13 +3,12 @@ import { Link } from 'react-router-dom'
 import { MdOutlineMenu } from "react-icons/md";
 import { IoMdClose } from "react-icons/io";
 const Header = () => {
-    const [click, setClick] = useState(true)
+    
     const [toggle, setToggle] = useState(false)
 
     const handleClick = () =>{
-        setClick((prevClick) => !prevClick )
-        setToggle(!toggle)
-        console.log(click)
+        setToggle((prevClick) => !prevClick )
+        console.log(toggle)
     }
   return (
     <React.Fragment>
@@ -18,7 +17,7 @@ const Header = () => {
                 <div id="logo">
                     <h1>HomeEstate</h1>
                 </div>
-                <div id='navs' className={`md:min-h-fit sm:absolute sm:hidden md:block md:static z-10 top-[60px] sm:${toggle ? 'block' : 'hidden'} sm:bg-gray-500 left-0 md:w-auto w-full flex items-center px-5`}>
+                <div id='navs' className={`md:min-h-fit sm:absolute  md:block md:static z-10 top-[60px] sm:${ toggle ? 'block' : 'hidden'} sm:bg-gray-500 left-0 md:w-auto w-full flex items-center px-5`}>
                     <ul className=' flex md:flex-row flex-col md:items-center md:gap-[4w] gap-8'>
                         <li>
                             <Link  to="/">HOME</Link>
@@ -39,7 +38,7 @@ const Header = () => {
                 </div>
                 <div className='md:hidden text-2xl cursor-pointer top[9%]'>
                     {
-                        click ? <MdOutlineMenu  onClick={handleClick}/>
+                        !toggle ? <MdOutlineMenu  onClick={handleClick}/>
                         :
                         <IoMdClose onClick={handleClick}/>
                     }
